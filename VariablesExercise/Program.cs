a268db35c39828fab01d19b4b1cc719cdfe67911
Caleb <cedinsmore63@gmail.com>
2025-08-12 22:43:42 -0400

    incomplete project

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string Myname = "Caleb";
+            int age = 23;
+            char gender = 'M';
+            bool doinggood = true;
+            double daystobday = 46;
+            decimal temp = new decimal(98.00);
+
+            Console.WriteLine($"My name is {Myname} and i am {age}. My sex is {gender} and i am " +
+                              "currently doing good {doinggood}. There are {daystobday} days till my birthday." +
+                              "And it was {decimal} degrees fahrenheit outside today.") ;
+        }
         }
     }
-}
+
